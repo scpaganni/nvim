f65a0a2aabb8e167fff8727fca3419cbe7bd201f
scpaganni <scpaganni@gmail.com>
2022-06-24 19:02:23 -0400

    Refazendo configuração do treesitter

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -4,29 +4,26 @@ if not status_ok then
 end
 
 require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all"
-  ensure_installed = { "all" },
+    ensure_installed = {},
+    sync_install = false,
+    ignore_install = {},
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
+    },
+    context_commentstring = {
+      enable = true,
+      enable_autocmd = false,
+    },
+    rainbow = {
+      enable = true,
+      disable = { "html" },
+      extended_mode = false,
+      max_file_lines = nil,
+    },
+    autopairs = { enable = true },
+    autotag = { enable = true },
+    incremental_selection = { enable = true },
+    indent = { enable = false },
+  }
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
-
-  -- List of parsers to ignore installing (for "all")
-  ignore_install = { "javascript" },
-
-  highlight = {
-    -- `false` will disable the whole extension
-    enable = true,
-
-    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-    -- the name of the parser)
-    -- list of language that will be disabled
-    disable = { "c", "rust" },
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
-}
