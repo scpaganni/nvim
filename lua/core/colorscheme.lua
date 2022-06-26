170f9c3dfa4a78af58d23402605c67abe5897d62
scpaganni <scpaganni@gmail.com>
2022-06-25 21:58:56 -0400

    Arrumando configuração do arquivo colorscheme

--- lua/core/colorscheme.lua
+++ lua/core/colorscheme.lua
@@ -1,6 +1,7 @@
-local status_ok, color_scheme = pcall(require, 'dracula')
+local colorscheme = "dracula"
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
+  vim.notify("colorscheme " .. colorscheme .. " not found!")
   return
 end
-
-vim.cmd[[colorscheme dracula]]
