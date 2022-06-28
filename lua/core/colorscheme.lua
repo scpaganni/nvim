8142b83894790ec317c242ab4caee9389ff7a173
scpaganni <scpaganni@gmail.com>
2022-06-28 13:29:07 -0400

    Instalando onedark theme

--- lua/core/colorscheme.lua
+++ lua/core/colorscheme.lua
@@ -1,7 +1,5 @@
-local colorscheme = "dracula"
-
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " not found!")
-  return
-end
+-- Lua
+require('onedark').setup {
+    style = 'deep'
+}
+require('onedark').load()
